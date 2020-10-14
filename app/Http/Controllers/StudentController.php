6bcc0914564f1a4a8b33963a9156639bcc84acf8
Vincent9o9 <66362554+Vincent9o9@users.noreply.github.com>
2020-10-14 18:10:20 +0200

    database

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -3,45 +3,55 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Student;
 
 class StudentController extends Controller
 {
-    protected $students;
-
-    public function __construct() {
-        $this->students = config('students');
-    }
-    public function studenti() {
-        $data = config('students');
+    public function studenti(){
+        $data = Student::all();
         return view('studenti', compact('data'));
     }
+
     public function show($id) {
-        if(!array_key_exists($id, $this->students)) {
-            abort(404);
-        }
-
-        $student = $this->students[$id];
-        return view('show',compact('student'));
+        $student = Student::find($id);
+        return view('show', compact('student'));
     }
-
-    //oppure utilizzo slug
-
-    public function slug($nome){
-        $trovato = false;
-        foreach ($this->students as $student){
-            if($student['slug'] == $nome) {
-                // se trova il valore lo memorizza in una variabile
-                $student = $studente;
-                // imposta trovato a true
-                $trovato = true;
-            }
-        }
-        if($trovato){
-            // se ha trovato il valore
-            return view('slug',compact('student'));
-        } else {
-            // se non ha trovato il valore
-            abort(404);
-        }
-    }
+    // protected $students;
+    //
+    // public function __construct() {
+    //     $this->students = config('students');
+    // }
+    // public function studenti() {
+    //     $data = config('students');
+    //     return view('studenti', compact('data'));
+    // }
+    // public function show($id) {
+    //     if(!array_key_exists($id, $this->students)) {
+    //         abort(404);
+    //     }
+    //
+    //     $student = $this->students[$id];
+    //     return view('show',compact('student'));
+    // }
+    //
+    // //oppure utilizzo slug
+    //
+    // public function slug($nome){
+    //     $trovato = false;
+    //     foreach ($this->students as $student){
+    //         if($student['slug'] == $nome) {
+    //             // se trova il valore lo memorizza in una variabile
+    //             $student = $studente;
+    //             // imposta trovato a true
+    //             $trovato = true;
+    //         }
+    //     }
+    //     if($trovato){
+    //         // se ha trovato il valore
+    //         return view('slug',compact('student'));
+    //     } else {
+    //         // se non ha trovato il valore
+    //         abort(404);
+    //     }
+    // }
 }
--- resources/views/studenti.blade.php
+++ resources/views/studenti.blade.php
@@ -4,14 +4,15 @@
     <div class="container card-group">
         @foreach ($data as $key => $student)
             <div class="card">
-                <img src="{{ $student['img']}}" class="card-img-top" alt="{{ $student['nome']}}">
+                <img src="{{ $student->img}}" class="card-img-top" alt="{{ $student->nome}}">
                 <div class="card-body">
-                    <a href="{{ route('Student.show', ['id' => $key] )}}">
-                        <h5 class="card-title">{{ $student['nome']}}</h5>
+                    <a href="{{ route('Student.show', ['id'=>$student->id ] )}}">
+                        <h5 class="card-title">{{ $student->name }}</h5>
                     </a>
-                    <p class="card-text">{{ $student['anni']}} Anni</p>
-                    <p class="card-text">Assunti{{ ($student['genere'] == 'f') ? 'a' : 'o' }} da {{ $student['azienda']}}</p>
+                    <p class="card-text">{{ $student->anni }} Anni</p>
+                    <p class="card-text">Assunti{{ ($student->genere == 'f') ? 'a' : 'o' }} da {{ $student->azienda}}</p>
                     {{-- <p class="card-text">{{ $student['descrizione']}}</p> --}}
+                    <p class="card-text">{{ $student->descrizione}}</p>
                 </div>
             </div>
       @endforeach
